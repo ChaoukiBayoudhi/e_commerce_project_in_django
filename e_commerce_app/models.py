3c3ff4356ea391b9204bbdfdcb49ec1d1f66de81
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-03-14 11:06:34 +0100

    second commit

--- e_commerce_app/models.py
+++ e_commerce_app/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from datetime import date
+from django.utils import timezone
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50,default='')
+    password = models.CharField(max_length=50,default='')
+    email = models.EmailField(default='')
+    phone = models.CharField(max_length=20,default='')
+    class Meta:
+        abstract=True
+        oredering=['email']
+    
+class Provider(User):
+    site_url=models.URLField(default='')
+    class Meta:
+        db_table='providers'
 class Product(models.Model):
     #id = models.BigAutoField(primary_key=True)
     label = models.CharField(max_length=100,default='')
@@ -9,11 +23,27 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images/product_images',null=True,blank=True)
     description = models.TextField(null=True,blank=True)
     expirationDate=models.DateField(default=date(2023,12,31))
-    fabricationDate=models.DateField(default=date.today())
-
-class Client(models.Model):
-    name=models.CharField(max_length=100,default='')
+    fabricationDate=models.DateField(default=timezone.now)
+    provider=models.ForeignKey(Provider,on_delete=models.CASCADE)
+    class Meta:
+        db_table='products'
+        #tuples in products table are ordered by label in ascending
+        #and then by price in descending order
+        ordering=['label','-price']
+class Client(User):
     familyName=models.CharField(max_length=100,default='')
-    email=models.EmailField(null=True,blank=True)
-    phone=models.CharField(max_length=20,default='+21622000000')
     typeClient=models.CharField(max_length=50,choices=[('LOYAL','Loyal Customer'),('NORMAL','Normal Customer'),('VIP','Very Import Customer')],default='NORMAL')
+    client_products=models.ManyToManyField(Product,through='Command',through_fields=('client','product'))
+    class Meta:
+        db_table='clients'
+class Command(models.Model):
+    client = models.ForeignKey(Client,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    date_cmd=models.DateField(default=timezone.now)
+    quantity=models.PositiveSmallIntegerField(default=1)
+    amount=models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    class Meta:
+        db_table='commands'
+        ordering=['-date_cmd']
+
+
--- e_commerce_project/settings.py
+++ e_commerce_project/settings.py
@@ -78,6 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'e_ecomerce_db',
+        # 'USER':'user_inga',
+        # 'PASSWORD':'user_inga',
+        # 'HOST':'localhost',
+        # 'PORT':5432,
     }
 }
 
