75ad42d4cfec2039f5ea169e72e8e31ce85d046f
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-03-15 11:27:26 +0100

    second commit

--- e_commerce_app/models.py
+++ e_commerce_app/models.py
@@ -3,18 +3,25 @@ from datetime import date
 from django.utils import timezone
 # Create your models here.
 class User(models.Model):
+
     name = models.CharField(max_length=50,default='')
     password = models.CharField(max_length=50,default='')
     email = models.EmailField(default='')
     phone = models.CharField(max_length=20,default='')
     class Meta:
         abstract=True
-        oredering=['email']
+        ordering=['email']
+    def __str__(self):
+        return f'name={self.name}, email={self.email}, phone={self.phone}'
     
 class Provider(User):
     site_url=models.URLField(default='')
     class Meta:
         db_table='providers'
+    def __str__(self):
+        #return f'name={self.name}, email={self.email}, phone={self.phone}, site_url={self.site_url}'
+        #or
+        return super().__str__()+f', site_url={self.site_url}'
 class Product(models.Model):
     #id = models.BigAutoField(primary_key=True)
     label = models.CharField(max_length=100,default='')
@@ -24,12 +31,24 @@ class Product(models.Model):
     description = models.TextField(null=True,blank=True)
     expirationDate=models.DateField(default=date(2023,12,31))
     fabricationDate=models.DateField(default=timezone.now)
-    provider=models.ForeignKey(Provider,on_delete=models.CASCADE)
+    #on_delete have many options: CASCADE, PROTECT, SET_NULL, SET_DEFAULT, SET(), DO_NOTHING
+    #CASCADE: when the referenced object is deleted, also delete the objects that have references to it
+    #PROTECT: when the referenced object is deleted, raise a ProtectedError without deleting the object
+    #SET_NULL: when the referenced object is deleted, set the forign key column to null
+    #SET_DEFAULT: when the referenced object is deleted, set the forign key column to the default value
+    #SET(): when the referenced object is deleted, set the forign key column to the value passed as parameter
+    #DO_NOTHING: when the referenced object is deleted, do nothing
+    provider=models.ForeignKey(Provider,on_delete=models.SET_NULL, null=True, blank=True)
     class Meta:
         db_table='products'
         #tuples in products table are ordered by label in ascending
         #and then by price in descending order
         ordering=['label','-price']
+    def __str__(self):
+        #return 'label = ',self.label,', price = ',self.price,', stock = ',self.stock
+        #return 'label = {}, price = {}, stock = {}'.format(self.label,self.price,self.stock)
+        #return 'label=%s, price=%s, stock=%s'%(self.label,self.price,self.stock)
+        return f'label={self.label}, price={self.price}, stock={self.stock}'
 class Client(User):
     familyName=models.CharField(max_length=100,default='')
     typeClient=models.CharField(max_length=50,choices=[('LOYAL','Loyal Customer'),('NORMAL','Normal Customer'),('VIP','Very Import Customer')],default='NORMAL')
@@ -43,7 +62,15 @@ class Command(models.Model):
     quantity=models.PositiveSmallIntegerField(default=1)
     amount=models.DecimalField(max_digits=10,decimal_places=2,default=0)
     class Meta:
+        #the table name in database is commands
         db_table='commands'
+        #tuples in commands table are ordered by date_cmd in descending order
         ordering=['-date_cmd']
+        #the name of the table in admin panel is Command table (a name readable by humans)
+        verbose_name='Command table'
+        #the name of plural objects in admin panel is Commands List (a name readable by humans)
+        verbose_name_plural='Command List'
+        #the combination of client, product and date_cmd must be unique
+        unique_together=[('client','product','date_cmd')]
 
 
--- e_commerce_project/settings.py
+++ e_commerce_project/settings.py
@@ -76,14 +76,13 @@ WSGI_APPLICATION = 'e_commerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'e_ecomerce_db',
-        # 'USER':'user_inga',
-        # 'PASSWORD':'user_inga',
-        # 'HOST':'localhost',
-        # 'PORT':5432,
+        
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'e_commerce_db',
+         'USER':'user_inga',
+         'PASSWORD':'user_inga',
+         'HOST':'localhost',
+         'PORT':5432,
     }
 }
 
